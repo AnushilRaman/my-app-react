e574857d49cea8f49c47f7a8107855528bbc607e
Anushil Raman <anushilraman1993@gmail.com>
2023-12-16 03:05:40 +0530

    Implementation of JSX basics in components

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <section>
           <Gallery />
-          <Profile/>
+          <Profile />
+          <TodoList/>
         </section>
       </header>
     </div>
@@ -18,3 +19,24 @@ function App() {
 }
 
 export default App;
+
+export function TodoList() {
+  return (
+    <diV>
+    // This doesn't quite work!
+      <h1>Hedy Lamarr's Todos</h1>
+      <img
+        src="https://i.imgur.com/yXOvdOSs.jpg"
+        alt="Hedy Lamarr"
+        class="photo"
+      ></img>
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul>
+    </diV>
+  );
+
+}
+
