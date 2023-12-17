527f0fc3b6be7afd8e34af874c46973ae5550ac4
Anushil Raman <anushilraman1993@gmail.com>
2023-12-18 00:05:46 +0530

    Passing an array for ins string to get awards

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,61 +1,65 @@
 import { getImageUrl } from "./utils.js";
 
 function Profile({
-    imageId,
-    name,
-    profession,
-    awards,
-    discovery,
-    imageSize = 70,
+  imageId,
+  name,
+  profession,
+  awards,
+  discovery,
+  imageSize = 70,
 }) {
-    return (
-        <section className="profile">
-            <h2>{name}</h2>
-            <img
-                className="avatar"
-                src={getImageUrl({imageId})}
-                alt={name}
-                width={imageSize}
-                height={imageSize}
-            />
-            <ul>
-                <li>
-                    <b>Profession: </b>
-                    {profession}
-                </li>
-                <li>
-                    <b>Awards: awards.length </b>
-                    {awards}
-                </li>
-                <li>
-                    <b>Discovered: </b>
-                    {discovery}
-                </li>
-            </ul>
-        </section>
-    );
+  return (
+    <section className="profile">
+      <h2>{name}</h2>
+      <img
+        className="avatar"
+        src={getImageUrl({imageId})}
+        alt={name}
+        width={imageSize}
+        height={imageSize}
+      />
+      <ul>
+        <li>
+          <b>Profession: </b>
+          {profession}
+        </li>
+        <li>
+          <b>Awards: {awards.length} </b>
+          {awards}
+        </li>
+        <li>
+          <b>Discovered: </b>
+          {discovery}
+        </li>
+      </ul>
+    </section>
+  );
 }
 
 export default function Gallery() {
-    return (
-        <div>
-            <h1>Notable Scientists</h1>
-            <Profile
-                imageId={'szV5sdG'}
-                name={"Maria Skłodowska-Curie"}
-                profession={"physicist and chemist"}
-                awards={'Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal,Matteucci Medal'}
-                discovery={"polonium (chemical element)"}
-                imageSize={70}
-            />
-            <Profile
-                imageId={"YfeOqp2"}
-                name={"Katsuko Saruhashi"}
-                profession={"geochemist"}
-                awards={"Miyake Prize for geochemistry, Tanaka Prize"}
-                discovery={"a method for measuring carbon dioxide in seawater"}
-                imageSize={70}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <h1>Notable Scientists</h1>
+      <Profile
+        imageId={"szV5sdG"}
+        name={"Maria Skłodowska-Curie"}
+        profession={"physicist and chemist"}
+        awards={[
+          "Nobel Prize in Physics",
+          "Nobel Prize in Chemistry",
+          "Davy Medal,Matteucci Medal",
+        ]}
+        discovery={"polonium (chemical element)"}
+        imageSize={70}
+      />
+      <Profile
+        imageId={"YfeOqp2"}
+        name={"Katsuko Saruhashi"}
+        profession={"geochemist"}
+        awards={["Miyake Prize for geochemistry", "Tanaka Prize"]}
+        discovery={"a method for measuring carbon dioxide in seawater"}
+        imageSize={70}
+      />
+    </div>
+  );
 }
