d69e3ad6117c8a2efa7266c249c1b732cf6680be
Anushil Raman <anushilraman1993@gmail.com>
2023-12-17 23:10:00 +0530

    Implementing clock on App.js by using props and useState hook

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Gallery from './Components/Gallery.js';
 import Profile from './Components/Profile.js';
+import Clock from './Components/clock.js';
+
+function useTime() {
+  const [time, setTime] = useState(() => new Date());
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+  return time
+}
+
 
 function App() {
+  const time = useTime();
+  const [color, setColor] = useState('lightcoral');
+
   return (
     <div className="App">
-     <section>
-          <Gallery />
-          <Profile />
-          {/* <TodoList /> */}
-        </section>
+      <section>
+        <Gallery />
+        <Profile />
+        {/* <TodoList /> */}
+      </section>
+      <p>
+        Pick a color.
+        <select value={color} onChange={e => setColor(e.target.value)}>
+          <option value="lightcoral">lightcoral</option>
+          <option value="midnightblue">midnightblue</option>
+          <option value="rebeccapurple">rebeccapurple</option>
+        </select>
+      </p>
+      <Clock color={color} time={time.toLocaleDateString() + ' ' +time.toLocaleTimeString()} />
     </div>
   );
 }
---
+++ src/Components/clock.js
@@ -0,0 +1,10 @@
+
+export default function clock({ color, time }) {
+    return (
+
+        <h1 style={{ color: color }}>
+            {time}
+        </h1>
+
+    );
+}
