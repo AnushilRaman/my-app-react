e95d92fd91f9f3b580ee8cb11fc9d6eaa3244524
Anushil Raman <anushilraman1993@gmail.com>
2023-12-21 01:08:07 +0530

    Fixing an event handler

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import { Form } from './Components/Form.js';
 import Counter from './Components/Counter.js';
 import Gallery from './Components/Gallery.js';
 import { BucketList } from './Components/BucketList.js';
+import { LightSwitch } from './Components/eventHandler.js';
 
 
 export default function App() {
 
   return (
     <>
+      <LightSwitch />
+      <br /><br /><br />
       <Gallery />
-      <Counter/>
-      <Form/>
-      <BucketList/>
+      <Counter />
+      <Form />
+      <BucketList />
+
     </>
   );
 }
